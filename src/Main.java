6b52497c9630c8c08fefe394f0e689dddd3c30b9
Anıl Cem Kaya <anlcemkaya@hotmail.com>
2023-06-09 23:30:55 +0300

    Generic Multi Methods

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String [] str={"anil","Kaya"};
+        Float [] flo={3.1f,4.3f};
+        Integer[] in={1,2,3};
+        run.print(flo);
+
+    }
+}
---
+++ src/run.java
@@ -0,0 +1,8 @@
+public class run {
+    public static <T> void print(T []arr){
+    for(T i:arr){
+        System.out.println(i);
+    }
+
+    }
+}
